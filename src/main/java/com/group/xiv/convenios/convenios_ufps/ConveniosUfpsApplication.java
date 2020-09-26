4334798acf2d2d1d13f046e48252d8f4cac06d66
JavierGarcia <jbgm93@gmail.com>
2020-09-26 11:55:58 -0500

    Creando repositorio

---
+++ src/main/java/com/group/xiv/convenios/convenios_ufps/ConveniosUfpsApplication.java
@@ -0,0 +1,13 @@
+package com.group.xiv.convenios.convenios_ufps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConveniosUfpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConveniosUfpsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group/xiv/convenios/convenios_ufps/ConveniosUfpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.xiv.convenios.convenios_ufps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConveniosUfpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
